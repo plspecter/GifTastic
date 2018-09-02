196c3caa275db23d28a4ca7a0f904e99c9d518d7
Perri Specter <plspecter@gmail.com>
2018-09-02 14:58:53 -0400

    GifTastic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,6 +9,21 @@ var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
 
 var topics = ["Pepe", "Patrick", "Danny DeVito"]
 
+//we need to render these buttons
+
+function renderButtons() {
+
+    for (var i = 0; i < topics.length; i++); {
+        //Makes the topics into buttons
+        var a = $("<button>");
+        //Add a class to define the topics
+        a.addClass("topic")
+        //Set text on the page to whatever gif u type in
+        a.text(topics[i])
+    }
+    
+}
+
 //Ajax time
 $.ajax({
 url: queryURL,
